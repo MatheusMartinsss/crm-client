82a08b16b9d4d662b46af006d83d2f48555d09a1
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-01-24 10:30:54 -0300

    create remote auth use case

---
+++ src/domain/useCases/remote-auth-useCase.js
@@ -0,0 +1,14 @@
+import Api from "../../helpers/api"
+import { ServerError, UnauthorizedError } from "../errors/errors"
+import { httpStatusCode } from "../protocols/http-response"
+export const authUseCase = async (email, password) => {
+    try {
+        const response = await Api.post('/auth', { email, password })
+        return response.data
+    } catch (error) {
+        switch (error.response.status) {
+            case httpStatusCode.unauthorized: throw new UnauthorizedError()
+            default: throw new ServerError()
+        }
+    }
+}
