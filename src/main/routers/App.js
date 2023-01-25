68c2fb48c809fa19bcc5ca8a838e862b018a630b
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-01-25 20:35:43 -0300

    add verifyUser logged

--- src/main/routers/App.js
+++ src/main/routers/App.js
@@ -4,26 +4,41 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ProtectedRoute } from "../hooks/protected-route";
 import { RestrictedRoute } from "../hooks/restricted-route";
 import { useAuth } from "../../domain/context/useAuth";
+import { useEffect } from "react";
+import { loadToken, removeToken } from "../../presentation/hooks/acess-token";
+import { validateTokenUseCase } from "../../domain/useCases/remote-auth-useCase";
+import Layout from "../../presentation/components/layout/layout";
 function App() {
-  const { user } = useAuth()
+  const { user, setUser } = useAuth()
+  useEffect(() => {
+    const verifyUserAlreadyLogged = async () => {
+      const token = loadToken()
+      if (!token) return
+      await validateTokenUseCase(token)
+        .then((response) => {
+          setUser(response)
+        }).catch(() => {
+          removeToken()
+        })
+    }
+    verifyUserAlreadyLogged();
+    // eslint-disable-next-line
+  }, [])
   return (
     <BrowserRouter>
-      <Box
-        component="main"
-        minHeight='100vh'
-      >
+      <Layout>
         <Routes>
           <Route element={<ProtectedRoute isAllowed={!!user} />}>
             <Route path="/" element={<HomeView />} />
           </Route>
           <Route>
-            <Route element={<RestrictedRoute user={user}/>}>
+            <Route element={<RestrictedRoute user={user} />}>
               <Route path="auth" element={<AuthView />} />
             </Route>
           </Route>
         </Routes>
         <CssBaseline />
-      </Box>
+      </Layout>
     </BrowserRouter >
 
   );
