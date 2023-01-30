40732e740fc3e8fb2d5b9ddcb72664dacd56a77a
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-01-30 14:39:43 -0300

    add new negociacao button

--- src/presentation/views/home/home-view.js
+++ src/presentation/views/home/home-view.js
@@ -1,11 +1,14 @@
-import { Box } from '@mui/material'
+import { Box, Fab } from '@mui/material'
 import React from 'react'
 import NegociacoesTable from '../../components/negociacoes-list/negociacoes-table'
 
 export const HomeView = () => {
     return (
-        <Box flexGrow={1}>
+        <Box display='flex' justifyContent='space-between' flexDirection='column' gap={5}>
             <NegociacoesTable />
+            <Fab variant="extended" size="small" sx={{ width: 300 }} color="primary" aria-label="add">
+                Nova negociação
+            </Fab>
         </Box>
     )
 }
