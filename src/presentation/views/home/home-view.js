e9e0e604b2484fcf1c4573ce0024d39b1f30feca
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-02-01 10:29:00 -0300

    add negociacao modal to homeview

--- src/presentation/views/home/home-view.js
+++ src/presentation/views/home/home-view.js
@@ -1,14 +1,13 @@
-import { Box, Fab } from '@mui/material'
+import { Box } from '@mui/material'
 import React from 'react'
+import { NegociacaoModal } from '../../components/negociacao-form/negociacao-modal'
 import NegociacoesTable from '../../components/negociacoes-list/negociacoes-table'
 
 export const HomeView = () => {
     return (
         <Box display='flex' justifyContent='space-between' flexDirection='column' gap={5}>
             <NegociacoesTable />
-            <Fab variant="extended" size="small" sx={{ width: 300 }} color="primary" aria-label="add">
-                Nova negociação
-            </Fab>
+            <NegociacaoModal />
         </Box>
     )
 }
