801caff5de068d9854e9945b98a8334180f56099
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-02-24 15:45:58 -0300

    add buttons e modal to home view

--- src/presentation/views/home/home-view.js
+++ src/presentation/views/home/home-view.js
@@ -1,17 +1,34 @@
-import { Box, Button } from '@mui/material'
+import { Box, ButtonGroup, Button } from '@mui/material'
 import React, { useState } from 'react'
 import NegociacoesTable from '../../components/negociacoes-list/negociacoes-table'
 import NegociacoesKankanList from '../../components/kanban-list/negociacoes-kanban'
+import DensitySmallIcon from '@mui/icons-material/DensitySmall';
+import CalendarViewWeekOutlinedIcon from '@mui/icons-material/CalendarViewWeekOutlined';
+import { NegociacaoModal } from '../../components/negociacao-form/negociacao-modal';
 export const HomeView = () => {
-    const [list, setList] = useState(true)
+    const [listType, setListType] = useState('list')
+    const [open, setOpen] = useState(false)
+
+    const handleModal = () => setOpen((state) => !state)
     return (
         <Box display='flex' justifyContent='space-between' flexDirection='column' gap={5}>
-            <Button onClick={() => setList((state) => !state)}>List</Button>
-            {list ?
-                <NegociacoesTable />
-                :
-                <NegociacoesKankanList />
-            }
+            <Box
+                display='flex'
+                flexDirection='row'
+                gap={2}
+            >
+                <ButtonGroup size='small' variant="outlined" aria-label="outlined primary button group">
+                    <Button onClick={() => setListType('list')}>
+                        <DensitySmallIcon />
+                    </Button>
+                    <Button onClick={() => setListType('kanban')}>
+                        <CalendarViewWeekOutlinedIcon />
+                    </Button>
+                </ButtonGroup>
+                <Button onClick={handleModal} size='small' variant='contained' color='success'>+ Negocio</Button>
+            </Box>
+            {listType === 'list' ? (<NegociacoesTable />) : (<NegociacoesKankanList />)}
+            <NegociacaoModal open={open} handleModal={handleModal} />
         </Box>
     )
 }
