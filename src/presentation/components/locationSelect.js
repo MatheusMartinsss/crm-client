121e62da7b296628ebbc3be04b65bf72e2af4c79
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-04-17 23:51:53 -0300

    refactor locationSelect

--- src/presentation/components/locationSelect.js
+++ src/presentation/components/locationSelect.js
@@ -1,91 +1,86 @@
-import * as React from 'react';
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-import CircularProgress from '@mui/material/CircularProgress';
-import Api from '../../helpers/api';
-import { Grid } from '@mui/material';
+import { Autocomplete, TextField, Grid } from "@mui/material"
+import Api from "../../helpers/api"
+import { useEffect, useState } from "react"
 
-export default function LocationSelect({ estado, cidade, setCidade, setEstado }) {
-    const [options, setOptions] = React.useState([]);
-    const loading = open && options.length === 0;
+const LocationSelect = ({ handleLocation, initialValue }) => {
+    const [estados, setEstados] = useState([])
+    const [cidades, setCidades] = useState([])
+    const [location, setLocation] = useState({
+        estado: {
+            nome: '' || initialValue?.estado,
+            sigla: '' || initialValue?.uf
+        },
+        cidade: '' || initialValue?.cidade
+    })
 
-    React.useEffect(() => {
-        if (!options.length) {
-            const fetchStates = async () => {
-                const response = await Api.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
-                setOptions(response.data)
-            }
-            fetchStates()
-        }
+    const fetchStates = async () => {
+        const response = await Api.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+        setEstados(response.data)
+    }
+    const fetchCitys = async () => {
+        const response = await Api.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${location?.estado?.sigla}/municipios`)
+        setCidades(response.data)
+    }
+    useEffect(() => {
+        setCidades([])
+
         // eslint-disable-next-line
-    }, [open])
-
+    }, [location.estado])
+    useEffect(() => {
+        handleLocation(location)
+        // eslint-disable-next-line
+    }, [location])
     return (
-        <React.Fragment>
-            <Grid item xs={12}>
+        <Grid container spacing={2}>
+            <Grid item xs={12} >
                 <Autocomplete
-                    fullWidth
-                    defaultValue={initialValue || null}
-                    isOptionEqualToValue={(option, value) => option.nome === value.nome || option}
+                    options={estados}
+                    value={location.estado || null}
                     getOptionLabel={(option) => option.nome || option}
-                    options={options}
-                    loading={loading}
-                    onChange={(value, newValue) => {
-                        setEstado(newValue)
+                    isOptionEqualToValue={(option, value) => option.sigla === value.sigla}
+                    onChange={(_, newValue) => {
+                        setLocation((state) => ({ ...state, estado: newValue, cidade: '' }))
                     }}
                     renderInput={(params) => (
                         <TextField
-                            error={error}
-                            helperText={helperText}
                             {...params}
+                            onClick={fetchStates}
                             label="Estados"
                             autoComplete='new-password'
                             InputProps={{
                                 ...params.InputProps,
-                                endAdornment: (
-                                    <React.Fragment>
-                                        {loading ? <CircularProgress color="inherit" size={20} /> : null}
-                                        {params.InputProps.endAdornment}
-                                    </React.Fragment>
-                                ),
                             }}
                         />
                     )}
-                />
+                >
+                </Autocomplete>
             </Grid>
-            {estado &&
-                <Grid item xs={12}>
-                    <Autocomplete
-                        fullWidth
-                        defaultValue={initialValue || null}
-                        isOptionEqualToValue={(option, value) => option.nome === value.nome || option}
-                        getOptionLabel={(option) => option.nome || option}
-                        options={options}
-                        loading={loading}
-                        onChange={(value, newValue) => {
-                            setCidade(newValue)
-                        }}
-                        renderInput={(params) => (
-                            <TextField
-                                error={error}
-                                helperText={helperText}
-                                {...params}
-                                label="Estados"
-                                autoComplete='new-password'
-                                InputProps={{
-                                    ...params.InputProps,
-                                    endAdornment: (
-                                        <React.Fragment>
-                                            {loading ? <CircularProgress color="inherit" size={20} /> : null}
-                                            {params.InputProps.endAdornment}
-                                        </React.Fragment>
-                                    ),
-                                }}
-                            />
-                        )}
-                    />
-                </Grid>
-            }
-        </React.Fragment>
-    );
+            <Grid item xs={12}>
+                <Autocomplete
+                    options={cidades}
+                    disabled={!location.estado}
+                    value={location.cidade}
+                    getOptionLabel={(option) => option.nome || option}
+                    isOptionEqualToValue={(option, value) => option.nome === value}
+                    onChange={(_, newValue) => {
+                        setLocation((state) => ({ ...state, cidade: newValue.nome }))
+                    }}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            onClick={fetchCitys}
+                            label="Cidades"
+                            autoComplete='new-password'
+                            InputProps={{
+                                ...params.InputProps,
+                            }}
+                        />
+                    )}
+                >
+                </Autocomplete>
+            </Grid>
+        </Grid>
+    )
 }
+
+export default LocationSelect
