300829fc035697a0afba6574b4f262e31c29ca46
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-04-20 15:29:28 -0300

    add theme options

---
+++ src/presentation/components/layout/theme-options.js
@@ -0,0 +1,33 @@
+import { createTheme } from '@mui/material/styles';
+
+export const DarkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#4b58bf',
+        },
+        secondary: {
+            main: '#180018',
+        },
+        background: {
+            default: '#272727',
+            paper: '#0e0d0d',
+        },
+    },
+});
+
+export const LightTheme = createTheme({
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#1976d2',
+        },
+        secondary: {
+            main: '#9c27b0',
+        },
+        background: {
+            default: '#F7F6F6',
+            paper: '#fff',
+        },
+    },
+})
