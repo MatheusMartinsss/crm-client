fd6f3de5912540bf3d887939b982271b93ffb6be
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-04-20 15:28:29 -0300

    add theme to layout

--- src/presentation/components/layout/layout.js
+++ src/presentation/components/layout/layout.js
@@ -1,32 +1,28 @@
-import { Box, CssBaseline, Toolbar } from "@mui/material";
-import React, { useState } from "react";
-import { useAuth } from "../../../domain/context/useAuth";
+import { Box, CssBaseline } from "@mui/material";
+import React from "react";
 import Header from "./header";
 import { Sidebar } from "./sidebar";
-
+import { ThemeProvider } from '@mui/material/styles';
+import { DarkTheme } from "./theme-options";
 
 const Layout = ({ children }) => {
-    const { user } = useAuth()
+    return (
+        <ThemeProvider
+            theme={DarkTheme}
+        >
+            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                <CssBaseline />
+                <Sidebar />
+                <Box sx={{ flex: 1, gap: 5, display: 'flex', flexDirection: 'column' }}>
+                    <Header />
+                    <Box paddingLeft={2} paddingRight={2} flex={1}>
+                        {children}
+                    </Box>
+                </Box>
+            </Box >
+        </ThemeProvider>
+    )
 
-    const [open, setOpen] = useState(false)
-    const handleOpen = () => setOpen((state) => !state)
-
-    return user ?
-        <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            <Header open={open} />
-            <Sidebar open={open} handleDrawer={handleOpen} />
-            <Box
-                component='main'
-                sx={{  flex: 1, p: 3 }}>
-                <Toolbar />
-                {children}
-            </Box>
-        </Box >
-        :
-        <Box component="main" minHeight='100vh'>
-            {children}
-        </Box>
 }
 
 export default Layout
