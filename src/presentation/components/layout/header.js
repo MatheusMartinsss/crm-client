faf30aa60398210909eb6c72068d811c824c0ff1
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-04-20 15:28:19 -0300

    refactor header

--- src/presentation/components/layout/header.js
+++ src/presentation/components/layout/header.js
@@ -1,37 +1,31 @@
-import { Box, Button, Stack, Toolbar, Typography } from '@mui/material'
+import { Box, Button, Divider, Stack, Typography } from '@mui/material'
 import React from 'react'
 import { logout } from '../../hooks/logout'
 import { useAuth } from '../../../domain/context/useAuth'
-import { styled } from '@mui/material/styles';
-import MuiAppBar from '@mui/material/AppBar';
 
 
-const AppBar = styled(MuiAppBar)(() => ({
-    width: `calc(100% - 80px)`,
-}));
-const Header = ({ open }) => {
+const Header = () => {
     const { setUser, user } = useAuth()
     const handleLogout = () => logout(setUser)
 
     return (
-        <AppBar position="fixed" open={open}>
-            <Toolbar disableGutters >
-                <Box display='flex' justifyContent='space-between' flexGrow={1} padding={2}>
-                    <Typography
-                        variant='h6'
-                        component='div'
-                        noWrap
-                    >
-                        CRM-Client
-                    </Typography>
-                    <Stack display='flex' flexDirection='row' alignItems='center' gap={1} >
-                        <Typography variant='h6' noWrap>
-                            Bem vindo, {user.name}</Typography>
-                        <Button onClick={handleLogout} variant='contained'>Logout</Button>
-                    </Stack>
-                </Box>
-            </Toolbar>
-        </AppBar>
+        <Box display='flex' justifyContent='space-between' flexDirection='column' height='80px'  >
+            <Box display='flex' flexDirection='row' justifyContent='space-between'>
+                <Typography
+                    variant='h6'
+                    component='div'
+                    noWrap
+                >
+                    CRM-Client
+                </Typography>
+                <Stack display='flex' flexDirection='row' alignItems='center' gap={1} >
+                    <Typography variant='h6' noWrap>
+                        Bem vindo, {user.name}</Typography>
+                    <Button onClick={handleLogout} variant='contained'>Logout</Button>
+                </Stack>
+            </Box>
+            <Divider />
+        </Box>
     )
 }
 
