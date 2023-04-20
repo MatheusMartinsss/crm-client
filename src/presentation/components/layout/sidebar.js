823725212f88d25b995c4662d46b70a34844feb8
MatheusMartinsss <matheus.s.martins98@gmail.com>
2023-04-20 15:29:38 -0300

    refactor sidebar

--- src/presentation/components/layout/sidebar.js
+++ src/presentation/components/layout/sidebar.js
@@ -1,11 +1,8 @@
 
 import React from 'react'
-
-import { Box, Divider, ListItemIcon, ListItem, ListItemButton, ListItemText, Paper, Typography } from "@mui/material";
+import { Box, ListItemIcon, ListItem, ListItemButton, ListItemText, Paper, Typography, Divider } from "@mui/material";
 import { menuItens } from './menuItens'
-import { styled, ThemeProvider, createTheme } from '@mui/material/styles';
 import { useNavigate } from "react-router-dom";
-
 
 
 export const Sidebar = () => {
@@ -14,42 +11,24 @@ export const Sidebar = () => {
         <Box
             sx={{ display: 'flex' }}
         >
-            <ThemeProvider
-                theme={createTheme({
-                    components: {
-                        MuiListItemButton: {
-                            defaultProps: {
-                                disableTouchRipple: true,
-                            },
-                        },
-                    },
-                    palette: {
-                        mode: 'dark',
-                        primary: { main: 'rgb(102, 157, 246)' },
-                        background: { paper: 'rgb(5, 30, 52)' },
-                    },
-                })}
-            >
-                <Paper elevation={0} sx={{ maxWidth: 300, minHeight: '100vh', flexDirection: 'column' }}>
-                    <Box sx={{ padding: 2, textAlign: 'center' }}>
-                        <Typography fontSize={20} fontWeight='medium'>CRM-Client</Typography>
-                    </Box>
-                    <Divider />
-                    {menuItens.map((item, idx) => (
-                        <ListItem key={idx} >
-                            <ListItemButton
-                                sx={{ py: 0, minHeight: 32, color: 'rgba(255,255,255,.8)' }}
-                                onClick={() => navigate(item.path)} >
-                                <ListItemIcon sx={{ color: 'inherit' }}>
-                                    {item.icon}
-                                </ListItemIcon>
-                                <ListItemText primary={item.label}
-                                    primaryTypographyProps={{ fontSize: 14, fontWeight: 'medium', color: 'white' }}>{item.label}</ListItemText>
-                            </ListItemButton>
-                        </ListItem>
-                    ))}
-                </Paper>
-            </ThemeProvider>
+            <Paper elevation={2} sx={{ maxWidth: 300, minHeight: '100vh', flexDirection: 'column' }}>
+                <Box sx={{ padding: 2, textAlign: 'center' }} display='flex' height='80px'>
+                    <Typography fontSize={20} fontWeight='medium'>CRM-Client</Typography>
+                </Box>
+          
+                {menuItens.map((item, idx) => (
+                    <ListItem key={idx} >
+                        <ListItemButton
+                            onClick={() => navigate(item.path)} >
+                            <ListItemIcon >
+                                {item.icon}
+                            </ListItemIcon>
+                            <ListItemText primary={item.label}
+                                primaryTypographyProps={{ fontSize: 14, fontWeight: 'medium' }}>{item.label}</ListItemText>
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </Paper>
         </Box>
     )
 }
